6135f31309c6957ccf52fbc6ff9ad6d4e9f48b16
devolo13 <devolo13@gmail.com>
2023-03-27 16:19:51 -0500

    swapped to cookies for theme storage

--- script.js
+++ script.js
@@ -1,33 +1,51 @@
-// Use sessionStorage for theme cookie
 var userTheme = '';
 initializeTheme();
 
-function setSessionStorage(currentTheme) {
-  userTheme = currentTheme;
-  sessionStorage.setItem('theme', userTheme);
+function setThemeCookie(theme){
+  userTheme = theme;
+  const d = new Date();
+  d.setTime(d.getTime() + (365*24*60*60*1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = "theme=" + theme + ";" + expires + ";path=/";
+}
+
+function retrieveThemeCookie(){
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for (let i = 0; i < ca.length; i++){
+    let c = ca[i];
+    while (c.charAt(0) == ' '){
+      c = c.substring(1);
+    }
+    if (c.indexOf('theme=')==0){
+      userTheme = c.substring(6, c.length);
+      return userTheme;
+    }
+  }
+  return "";
 }
 
 function initializeTheme() {
-  userTheme = sessionStorage.getItem('theme');
+  retrieveThemeCookie();
   if (userTheme === 'light') {
     document.documentElement.setAttribute('data-bs-theme', 'light');
     document.getElementById('themeButton').innerHTML = '<i class="bi bi-brightness-alt-high-fill"></i>';
   } else {
-    setSessionStorage('dark');
+    setThemeCookie('dark');
     document.documentElement.setAttribute('data-bs-theme', 'dark');
     document.getElementById('themeButton').innerHTML = '<i class="bi bi-brightness-high-fill"></i>';
   }
 }
 
 function themeToggle(element) {
-  userTheme = sessionStorage.getItem('theme');
+  retrieveThemeCookie;
   if (userTheme == 'dark') {
     document.documentElement.setAttribute('data-bs-theme', 'light');
     element.innerHTML = '<i class="bi bi-brightness-alt-high-fill"></i>';
-    setSessionStorage('light');
+    setThemeCookie('light');
   } else {
     document.documentElement.setAttribute('data-bs-theme', 'dark');
     element.innerHTML = '<i class="bi bi-brightness-high-fill"></i>';
-    setSessionStorage('dark');
+    setThemeCookie('dark');
   }
 }
