09e6ec29e674f56cb82a49a2715e8916b31e5624
devolo13 <devolo13@gmail.com>
2023-03-30 15:05:44 -0500

    Cleaning and reorganizing

--- script.js
+++ script.js
@@ -49,3 +49,11 @@ function themeToggle(element) {
     setThemeCookie('dark');
   }
 }
+
+function themeSelect(element) {
+  var chosenTheme = 'resources/';
+  chosenTheme += element.value;
+  chosenTheme += '.bootstrap.min.css';
+  var theBootstrapStylesheet = document.getElementById('bootstrapStylesheet');
+  theBootstrapStylesheet.setAttribute('href', chosenTheme);
+}
--- test.js
+++
@@ -1,7 +0,0 @@
-function themeSelect(element) {
-  var chosenTheme = 'resources/';
-  chosenTheme += element.value;
-  chosenTheme += '.bootstrap.min.css';
-  var theBootstrapStylesheet = document.getElementById('bootstrapStylesheet');
-  theBootstrapStylesheet.setAttribute('href', chosenTheme);
-}
